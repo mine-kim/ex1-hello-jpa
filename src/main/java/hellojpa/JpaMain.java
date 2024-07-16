aae9b02443b474cbd64282141b2141df0411c3bc
kmh <minhye0619@gmail.com>
2024-07-17 00:22:34 +0900

    엔티티 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -19,18 +20,20 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member");
+            member.setTeam(team);
+
+            em.persist(member);
+
+            tx.commit();
+
            /* create*/
-            Member member1 = new Member();
-            member1.setName("A");
-            Member member2 = new Member();
-            member2.setName("B");
-            Member member3 = new Member();
-            member3.setName("C");
-            System.out.println("==============");
-            em.persist(member1);
-            em.persist(member2);
-            em.persist(member3);
-            System.out.println("==============");
+
             //select
             //Member findMember = em.find(Member.class, 1L);
             //System.out.println("findMember = " + findMember.getName());
@@ -40,8 +43,6 @@ public class JpaMain {
             
             //delete
             // em.remove(findMember);
-
-            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -9,10 +9,23 @@ initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-    generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
-    private String name;
+    @Column(name = "USERNAME")
+    private String username;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+      //  team.getMembers().add(this);
+    }
 
     public Long getId() {
         return id;
@@ -22,11 +35,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
